db86d936e43b7c35c472994464db5a56262957a2
Celeste JS <wonderlugosi@gmail.com>
2022-07-05 16:05:17 -0500

    useForm

--- landing-page/src/MyForms.tsx
+++ landing-page/src/MyForms.tsx
@@ -3,20 +3,33 @@ import { useForm } from "react-hook-form"
 export default function MyFormH () {
     const { register, handleSubmit } = useForm ();
 
+    const onSubmit = (data) => {
+        console.log(data);
+    }
+
     return (
         <>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
             <div>
                 <br />
                 <label>First Name:</label> <br />
-                <input type="text" name=""></input> <br />
+                <input type="text" {...register('name', {
+                    required: true,
+                    
+                })}></input> <br />
                 <br />
             </div>
             <div>
                 <label>Last Name:</label> <br />
-                <input type="text" name=""></input> <br />
+                <input type="select" {...register('Lname')}></input> <br />
                 <br />
+                <label>Select</label>
+                <select {...register('onumer')}>
+                    <option value="1"> 1</option>
+                    <option value="2"> 2</option>
+                </select>
             </div>
+                <input type="submit" value="Enviar" />
         </form>
         </>
     )
