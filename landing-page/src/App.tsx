5d9024a2bf874282a0ce81a6057cded952084075
Celeste JS <wonderlugosi@gmail.com>
2022-07-08 17:58:26 -0500

    switch try1

--- landing-page/src/App.tsx
+++ landing-page/src/App.tsx
@@ -1,12 +1,29 @@
-import Mybuilding from './components/myImg'
 import MyFormH from './components/MyForms'
-import { Contact } from './components/Contact'
-import { createTheme, NextUIProvider } from '@nextui-org/react'
+import { createTheme, Grid, NextUIProvider, Switch } from '@nextui-org/react'
 import { UnderConstruction } from './components/UnderConstruction'
+import sun from '@iconify/icons-ph/sun-dim'
+import moon from '@iconify/icons-ph/moon-stars'
+import { Icon } from '@iconify/react'
+import { useState } from 'react'
 
 function App() {
+  
+  const [val, setVal] = useState("ON")
+  function toggle() {
+    setVal((val==="ON")?"OFF":"ON");
+  }
   return (
     <NextUIProvider theme={createTheme({ type: 'dark' })}>
+      <Grid>
+        <Switch
+          onClick={toggle}
+          // checked={false}
+          size="xl"
+          iconOn={<Icon icon={sun} />}
+          iconOff={<Icon icon={moon} />}
+        />
+        {val}
+      </Grid>
       <div style={{ display: 'grid', placeContent: 'center', minHeight: '100vh' }}>
         <UnderConstruction />
         <br />
