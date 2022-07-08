95d39e4068c2199c6fc2e37189ae1a80ce19dd8a
Ricardo Valero de la Rosa <55701657+RicardoValero95@users.noreply.github.com>
2022-07-08 12:42:29 -0500

    App update

--- landing-page/src/App.tsx
+++ landing-page/src/App.tsx
@@ -1,18 +1,16 @@
-import { useState } from 'react'
-import './App.css'
 import Mybuilding from './components/myImg'
 import MyFormH from './components/MyForms'
 import { Contact } from './components/Contact'
+import { createTheme, NextUIProvider } from '@nextui-org/react'
+import { UnderConstruction } from './components/UnderConstruction'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div>
-          <Mybuilding></Mybuilding>
-          <MyFormH></MyFormH>
-          {/* <Contact></Contact> */}
-    </div>
+    <NextUIProvider theme={createTheme({ type: 'dark' })}>
+      <div style={{ display: 'grid', placeContent: 'center', minHeight: '100vh' }}>
+        <UnderConstruction />
+      </div>
+    </NextUIProvider >
   )
 }
 
