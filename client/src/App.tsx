c266668ed476b76f46b7cbf25b617617f690b76c
Celeste JS <wonderlugosi@gmail.com>
2022-07-11 13:59:23 -0500

    json

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,31 +4,25 @@ import { UnderConstruction } from './components/UnderConstruction'
 import sun from '@iconify/icons-ph/sun-dim'
 import moon from '@iconify/icons-ph/moon-stars'
 import { Icon } from '@iconify/react'
-
-const lightTheme = createTheme({
-  type: 'light',
-  theme: {
-  }
-})
-
-const darkTheme = createTheme({
-  type: 'dark',
-  theme: {
-  }
-})
-
-const darkMode = useDarkMode(false);
+import { useState } from 'react'
 
 function App() {
+  
+  const [val, setVal] = useState("ON")
+  function toggle() {
+    setVal((val==="ON")?"OFF":"ON");
+  }
   return (
     <NextUIProvider theme={createTheme({ type: 'dark' })}>
       <Grid>
         <Switch
-          checked={false}
+          onClick={toggle}
+          // checked={false}
           size="xl"
           iconOn={<Icon icon={sun} />}
           iconOff={<Icon icon={moon} />}
         />
+        {val}
       </Grid>
       <div style={{ display: 'grid', placeContent: 'center', minHeight: '100vh' }}>
         <UnderConstruction />
